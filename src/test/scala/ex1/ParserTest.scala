7d31db5cbdfb27474960f860cf91e77034ef5f96
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-22 11:09:54 +0200

    feat: add String parser test

--- src/test/scala/ex1/ParserTest.scala
+++ src/test/scala/ex1/ParserTest.scala
@@ -1,10 +1,14 @@
 package ex1
 
 import org.scalatest.matchers.should.Matchers.*
+import ex1.Parsers.charParser
 
 class ParserTest extends org.scalatest.funsuite.AnyFunSuite:
     def basicParser = new BasicParser(Set('a', 'b', 'c'))
     def notEmptyParser = new NonEmptyParser(Set('0', '1'))
+    def notTwoParser = new NotTwoConsecutiveParser(Set('X', 'Y', 'Z'))
+    def stringParser = "abc".charParser()
+
     test ("Test Basic Parser operations"):
         basicParser.parseAll("aabc".toList) should be (true)
         basicParser.parseAll("aabcdc".toList) should be (false)
@@ -14,5 +18,16 @@ class ParserTest extends org.scalatest.funsuite.AnyFunSuite:
         notEmptyParser.parseAll("0101".toList) should be (true)
         notEmptyParser.parseAll("0123".toList) should be (false)
         notEmptyParser.parseAll(List()) should be (false)
+
+    test("Test Not Two Consecutive Parser"):
+        notTwoParser.parseAll("XYZ".toList) should be (true)
+        notTwoParser.parseAll("XYYZ".toList) should be (false)
+        notTwoParser.parseAll(List()) should be (false)
+    
+    test("Test String Parser"):
+        stringParser.parseAll("aabc".toList) should be (true)
+        stringParser.parseAll("aabcdc".toList) should be (false)
+        stringParser.parseAll(("".toList)) should be (true)
+
 
 
