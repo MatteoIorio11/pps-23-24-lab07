4e321368b338a835c74fba867ac0b5c1129daaca
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-22 16:52:36 +0200

    feat: add tests for RobotCanFail

--- src/test/scala/ex2/TestRobotFail.scala
+++ src/test/scala/ex2/TestRobotFail.scala
@@ -3,3 +3,17 @@ import org.scalatest.matchers.should.Matchers.*
 
 class TestRobotFail extends org.scalatest.funsuite.AnyFunSuite:
     val robot = new RobotCanFail(new SimpleRobot((0,0), Direction.North), 100)
+    val luckyRobot = new RobotCanFail(new SimpleRobot((0,0), Direction.North), 0)
+
+    test("Test Robot in action super unlucky with 100% of act fail"):
+        robot.act()
+        robot.position should be ((0,0))
+        robot.act()
+        robot.position should be((0,0))
+    
+    test("Test Robot in action super lucky with 0% of act fail"):
+        luckyRobot.act()
+        luckyRobot.position should be ((0, 1))
+        luckyRobot.act()
+        luckyRobot.position should be ((0, 2))
+
