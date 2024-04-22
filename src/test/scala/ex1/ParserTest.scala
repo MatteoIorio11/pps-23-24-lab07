719a4393f0b84829ffa3350bc38aa70ae7944625
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-22 10:58:08 +0200

    feat: add scala test for BasicParser

--- src/test/scala/ex1/ParserTest.scala
+++ src/test/scala/ex1/ParserTest.scala
@@ -5,5 +5,7 @@ import org.scalatest.matchers.should.Matchers.*
 class ParserTest extends org.scalatest.funsuite.AnyFunSuite:
     def basicParser = new BasicParser(Set('a', 'b', 'c'))
     test ("Test Basic Parser operations"):
-        basicParser.parseAll("aabc") should be (true)
+        basicParser.parseAll("aabc".toList) should be (true)
+        basicParser.parseAll("aabcdc".toList) should be (false)
+        basicParser.parseAll("".toList) should be (true)
 
