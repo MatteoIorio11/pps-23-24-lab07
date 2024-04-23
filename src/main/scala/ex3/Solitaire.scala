487a252bf3415b541366594de77a4b2608bdb8c6
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-23 14:26:48 +0200

    feat: add Solitaire game logic

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,15 +1,51 @@
 package ex3
 
+import scala.collection.immutable.SortedSet
+type Board = (Int, Int)
+type BoardPosition = (Int, Int)
+type Solution = Iterable[BoardPosition]
+type IterableFactory = Solution => Iterable[Solution]
+given IterableFactory = SortedSet(_)
+
 object Solitaire extends App:
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
-    val reversed = solution.reverse
+  def placeMarks(board: Board)(using factory: IterableFactory): Iterable[Solution] = 
+    require(board._1 > 1)
+    require(board._2 > 1)
+    val goal = board._1 * board._2
+    this.solitaireLogic(board = board)(goal)
+
+
+  private def solitaireLogic(board: Board)(remainingPositions: Int)(using factory: IterableFactory): Iterable[Solution] = remainingPositions match
+    case 0 => factory(Set((board._1 / 2, board._2 / 2)))
+    case _ => 
+      for
+        positions <- solitaireLogic(board = board)(remainingPositions = remainingPositions - 1)
+        possibleX <- 0 until board._1
+        possibleY <- 0 until board._2
+        possiblePosition = (possibleX, possibleY)
+        if isPlaceable(possiblePosition = possiblePosition, positions = positions)
+      yield
+        positions.toSeq :+ possiblePosition
+  
+
+  private def isPlaceable(possiblePosition: BoardPosition, positions: Solution): Boolean = 
+    val lastPosition = positions.last
+    ((lastPosition._1 - possiblePosition._1).abs == 2 && (lastPosition._2 - possiblePosition._2).abs == 0 ||
+    (lastPosition._1 - possiblePosition._1).abs == 1 && (lastPosition._2 - possiblePosition._2).abs == 1)
+    
+  
+
+
+  def render(solution: Seq[BoardPosition], width: Int, height: Int): String =
+    //val reversed = solution.reverse
     val rows =
       for y <- 0 until height
           row = for x <- 0 until width
-          number = reversed.indexOf((x, y)) + 1
+          number = solution.indexOf((x, y)) + 1
           yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
 
-
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  //placeMarks((3,3)).foreach(p => p)
+  placeMarks((3,3)).toSeq.foreach(sol => println(render(sol.toSeq, 3, 3)))
+  //println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
