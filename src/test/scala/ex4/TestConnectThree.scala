9853f71dbfbd55d58be2443ebc75b8c92e5abe36
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-23 16:16:54 +0200

    feat: add Test find first available row

--- src/test/scala/ex4/TestConnectThree.scala
+++ src/test/scala/ex4/TestConnectThree.scala
@@ -11,4 +11,10 @@ class TestConnectThree extends org.scalatest.funsuite.AnyFunSuite:
         game.find(List(Disk(0, 0, X)), 0, 0) should be (Some(X))
         game.find(List(Disk(0, 0, X), Disk(0, 1, O), Disk(0, 2, X)), 0, 1) should be (Some(O))
         game.find(List(Disk(0, 0, X), Disk(0, 1, O), Disk(0, 2, X)), 1, 1) should be (None)
-        
+
+    test("Test Find First Available Row"):
+        game.firstAvailableRow(List(), 0) should be (Some(0))
+        game.firstAvailableRow(List(Disk(0,0, X)), 0) should be (Some(1))
+        game.firstAvailableRow(List(Disk(0, 0, X), Disk(0, 1, X), Disk(0, 2, X), Disk(0, 3, X)), 0) should be (None)
+        game.firstAvailableRow(List(Disk(0, 0, X), Disk(0, 1, X), Disk(0, 2, X), Disk(0, 3, X)), 1) should be (Some(0))
+
