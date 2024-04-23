edbf2fbc5f1c9b825c2912086df57bb15c519fe4
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-23 14:57:24 +0200

    feat: add find tests

--- src/test/scala/ex4/TestConnectThree.scala
+++ src/test/scala/ex4/TestConnectThree.scala
@@ -6,6 +6,7 @@ import ex4.ConnectThree.Player.*
 
 class TestConnectThree extends org.scalatest.funsuite.AnyFunSuite:
     val game = ConnectThree
+    
     test("Test Find method"):
         game.find(List(Disk(0, 0, X)), 0, 0) should be (Some(X))
         game.find(List(Disk(0, 0, X), Disk(0, 1, O), Disk(0, 2, X)), 0, 1) should be (Some(O))
