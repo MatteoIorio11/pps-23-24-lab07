4cd8f77420a8d940dc3e7eb0900f361e3a0d92ba
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-25 16:22:19 +0200

    feat: refactor implementation, use a Queue for each solution

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,40 +1,39 @@
 package ex3
 
 import scala.collection.immutable.SortedSet
+import scala.collection.immutable.Queue
 type Board = (Int, Int)
 type BoardPosition = (Int, Int)
-type Solution = Iterable[BoardPosition]
+type Solution = Queue[BoardPosition]
 type IterableFactory = Solution => Iterable[Solution]
-given IterableFactory = SortedSet(_)
+given IterableFactory = LazyList(_)
 
 object Solitaire extends App:
   def placeMarks(board: Board)(using factory: IterableFactory): Iterable[Solution] = 
-    require(board._1 > 1)
-    require(board._2 > 1)
+    require(board._1 >= 3)
+    require(board._2 >= 3)
     val goal = board._1 * board._2
     this.solitaireLogic(board = board)(goal)
 
 
   private def solitaireLogic(board: Board)(remainingPositions: Int)(using factory: IterableFactory): Iterable[Solution] = remainingPositions match
-    case 0 => factory(Set((board._1 / 2, board._2 / 2)))
+    case 0 => factory(Queue((board._1 / 2, board._2 / 2)))
     case _ => 
       for
         positions <- solitaireLogic(board = board)(remainingPositions = remainingPositions - 1)
         possibleX <- 0 until board._1
         possibleY <- 0 until board._2
         possiblePosition = (possibleX, possibleY)
-        if isPlaceable(possiblePosition = possiblePosition, positions = positions)
+        if positions.forall(pos => !pos.eq(possiblePosition)) && isPlaceable(possiblePosition = possiblePosition, positions = positions)
       yield
-        positions.toSeq :+ possiblePosition
+        positions.enqueue(possiblePosition)
   
 
   private def isPlaceable(possiblePosition: BoardPosition, positions: Solution): Boolean = 
     val lastPosition = positions.last
     ((lastPosition._1 - possiblePosition._1).abs == 2 && (lastPosition._2 - possiblePosition._2).abs == 0 ||
     (lastPosition._1 - possiblePosition._1).abs == 1 && (lastPosition._2 - possiblePosition._2).abs == 1)
-    
-  
-
+ 
 
   def render(solution: Seq[BoardPosition], width: Int, height: Int): String =
     //val reversed = solution.reverse
@@ -45,7 +44,5 @@ object Solitaire extends App:
           yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
-
-  //placeMarks((3,3)).foreach(p => p)
-  placeMarks((3,3)).toSeq.foreach(sol => println(render(sol.toSeq, 3, 3)))
+  placeMarks((3, 3)).foreach(p => println(p))
   //println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
